63bd58cc411144d34f0e93a3ef3bbd8084c11cd4
albedard <albedard@my.uri.edu>
2016-03-05 23:33:47 -0500

    commit fileIO.h

---
+++ prog01/fileIO.h
@@ -0,0 +1,28 @@
+#ifndef	FILE_IO_H
+#define	FILE_IO_H
+
+typedef enum eimageFileType
+{
+		kUnknownImageType = -1,
+		kTGA_COLOR,				//	24-bit color image
+		kTGA_GRAY,
+		kPPM,					//	24-bit color image
+		kPGM					//	8-bit gray-level image
+} eImageFileType;
+
+typedef enum efileErrorCode
+{
+		kNoIOerror = 0,
+		kFileNameNull,
+		kFileNotFound,
+		kCannotOpenWrite,
+		kCannotOpenRead,
+		kWrongFileType,
+		kUnknownFileType,
+		kErrorReading,
+		kErrorWriting,
+		kEndOfFileError,
+		kMemAllocError
+} eFileErrorCode;
+
+#endif
